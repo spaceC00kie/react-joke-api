bbac444b678d415af36e432138c0db60347a6c11
spaceC00kie <kirstie317@gmail.com>
2021-01-29 03:24:39 -0500

    finishing up

--- cnjokes/src/Components/Chuck.js
+++ cnjokes/src/Components/Chuck.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from "react"
 
 const Chuck = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [joke, setJoke] = useState("Click for a joke")
+
+  
+  const updateJoke = () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((res) => res.json())
+      .then((i) => setJoke(i.value))
+  }
+
+  return (
+    <div>
+      <button onClick={() => updateJoke()}>New Joke</button>
+      {joke}
+    </div>
+  )
 }
 
 export default Chuck
