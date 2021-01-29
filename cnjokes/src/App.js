c65c5a4ecd8823f97a61f35067036b5cf060108e
spaceC00kie <kirstie317@gmail.com>
2021-01-28 23:34:44 -0500

    set up structure

---
+++ cnjokes/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Chuck from './Components/Chuck';
+
+function App() {
+  return (
+    <div>
+      <Chuck/>
+    </div>
+  );
+}
+
+export default App;
---
+++ cnjokes/src/Components/Chuck.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chuck = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chuck
